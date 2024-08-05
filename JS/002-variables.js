a7d610b5853a91465dda1114ed6f90220be39630
VirusARzk-Git <aditya.21ug1001@iiitranchi.ac.in>
2024-08-05 23:24:37 +0530

    JS

---
+++ JS/002-variables.js
@@ -0,0 +1,16 @@
+const regNo = '2021UG1001'
+// cannot reassign a constant variable
+
+let name = 'Aditya Raj'
+// scope of let is block level
+
+var age = 20
+// scope of var is function level
+// never use var, use let instead
+
+city = 'Deoghar'
+
+let phoneNo
+
+console.log(regNo, name, age, city, phoneNo)
+console.table({ regNo, name, age, city, phoneNo })
---
+++ JS/003-dataTypes.js
@@ -0,0 +1,62 @@
+"use strict"; // to enable strict mode (optional)
+// treat all JS code as newer version
+
+// alert('Hello, World!') // alert box
+
+// Data Types
+// 1. Primitive Data Types
+// 2. Reference Data Types
+
+// Primitive Data Types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+// String
+const name = 'Aditya Raj'
+console.log(name)
+console.log(typeof name)
+
+// Number
+const age = 20
+console.log(age)
+console.log(typeof age)
+
+// Boolean
+const isStudent = true
+console.log(isStudent)
+console.log(typeof isStudent)
+
+// Null
+const nullValue = null
+console.log(nullValue)
+console.log(typeof nullValue)
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+console.log(typeof undefinedValue)
+
+// Symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+// BigInt
+const bigInt = 1234567890123456789012345678901234567890n
+console.log(bigInt)
+console.log(typeof bigInt)
+
+// Reference Data Types
+// 1. Array
+// 2. Object
+// 3. Function
+// 4. Date
+// 5. RegExp
+// 6. Map, Set, WeakMap, WeakSet
+// 7. Error
+
