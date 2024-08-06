6396e974431736d6b8e9bc0843339d235ec6e193
VirusARzk-Git <aditya.21ug1001@iiitranchi.ac.in>
2024-08-06 12:51:40 +0530

    JS

--- JS/009-string.js
+++ JS/009-string.js
@@ -12,9 +12,29 @@ console.log(name.toLowerCase());
 console.log(name.substring(0, 3));
 console.log(name.split(''));
 
-const goodName = new String('Aditya');
+const goodName = new String('Aaditya');
 console.log(typeof goodName); // object
-console.log(goodName); // [String: 'Aditya']
+console.log(goodName); // [String: 'Aaditya']
 console.log(goodName[0]); // A
-console.log(goodName.length); // 6
+console.log(goodName.length); // 7
 console.log(goodName.__proto__); // [String: '']
+
+console.log(goodName.charAt(0)); // A
+console.log(goodName.charCodeAt(0)); // 65
+console.log(goodName.concat(' Raj')); // Aditya Raj
+console.log(goodName.includes('A')); // true
+console.log(goodName.indexOf('a')); // 1
+console.log(goodName.lastIndexOf('a')); // 5
+
+const newString = goodName.substr(1, 3);
+console.log(newString); // adi
+
+const newString2 = goodName.slice(-5, 6);
+console.log(newString2); // dity
+
+const newString3 = new String("  Aaditya  ");
+console.log(newString3); // [String: '  Aaditya  ']
+console.log(newString3.trim()); // Aaditya
+
+const url = 'https://virusarzk.com/virus%20arzk';
+console.log(url.replace('%20', '-')); // https://virusarzk.com/virus arzk
