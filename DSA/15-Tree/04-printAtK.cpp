85e716e6895517800bc5854ea9a5c82966b1ee1f
VirusARzk-Git <aditya.21ug1001@iiitranchi.ac.in>
2024-10-20 12:20:56 +0530

    Tree

--- DSA/15-Tree/04-printAtK.cpp
+++ DSA/15-Tree/04-printAtK.cpp
@@ -1,19 +1,44 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
 {
 public:
-    int key;
-    Node *lc;
-    Node *rc;
+    int data;
+    Node *left;
+    Node *right;
 
     Node(int k)
     {
-        key = k;
-        lc = NULL;
-        rc = NULL;
+        data = k;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+    vector<int> ans;
+    int k;
+    void f(Node *root, int level)
+    {
+        if (!root)
+            return;
+        if (k == level)
+        {
+            ans.push_back(root->data);
+            return;
+        }
+        f(root->left, level + 1);
+        f(root->right, level + 1);
+    }
+
+public:
+    vector<int> Kdistance(struct Node *root, int k_)
+    {
+        k = k_;
+        f(root, 0);
+        return ans;
     }
 };
 
@@ -22,20 +47,20 @@ void printAtK(Node *root, int k)
     if (root == NULL)
         return;
     if (k == 0)
-        cout << root->key << ' ';
+        cout << root->data << ' ';
     else
     {
-        printAtK(root->lc, k - 1);
-        printAtK(root->rc, k - 1);
+        printAtK(root->left, k - 1);
+        printAtK(root->right, k - 1);
     }
 }
 
 int main()
 {
     Node *root = new Node(10);
-    root->lc = new Node(20);
-    root->lc->lc = new Node(40);
-    root->rc = new Node(30);
-    root->rc->rc = new Node(90);
+    root->left = new Node(20);
+    root->left->left = new Node(40);
+    root->right = new Node(30);
+    root->right->right = new Node(90);
     printAtK(root, 2);
 }
